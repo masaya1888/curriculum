ec3236fc80e89ce26b9d6111777aea92689d6503
masaya1888 <100127806+masaya1888@users.noreply.github.com>
2023-08-19 01:31:55 +0900

    第10章課題提出
    第10章チェックテスト

---
+++ JavaStudy/10-1/main.js
@@ -0,0 +1,27 @@
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+let num = numbers;
+function isEven(num) {
+    console.log(num + 'は偶数です');
+}
+for (let i = 0; i < num.length; i++) {
+    
+    if ( num[i] % 2 === 0) {  
+        
+        isEven(num[i]);
+    }
+}
+                    
+
+class car {
+    constructor() {
+        this.gass = gass;
+        this.num = num;
+    }    
+}
+
+function getNumGas(){
+
+    console.log("ガソリンは"+car.gass+"です。"+"ナンバーは"+car.num+"です");
+
+}
